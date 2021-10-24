861fd5e9b503f6d0efb3e2af22bafb8474900a98
Andrey D <49348855+turdusMerul@users.noreply.github.com>
2021-10-24 17:43:13 +0300

    Update settings.py

--- capos/capos/settings.py
+++ capos/capos/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)7q2vf@hbeswnnpwq69!^&8h#m*@ml6hka6@&43qnl_gsm3ai&'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
