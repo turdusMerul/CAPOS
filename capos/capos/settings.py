3ed3f9e0ae051a12d5e23b7187287a2f6d2490ed
Andrey D <49348855+turdusMerul@users.noreply.github.com>
2021-10-24 18:46:59 +0300

    Update settings.py

--- capos/capos/settings.py
+++ capos/capos/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
